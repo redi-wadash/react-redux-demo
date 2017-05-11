6129ede35cb2db0eb60f1625a0fd6fc89cd16398
Shogo Wada <shogo.wada@redi.com>
2017-05-11 09:07:43 -0500

    Derive next ID from current state

--- src/reducers/todos.js
+++ src/reducers/todos.js
@@ -1,34 +1,37 @@
-const todo = (state = {}, action) => {
+const todos = (state = [], action) => {
   switch (action.type) {
     case 'ADD_TODO':
-      return {
-        id: action.id,
+      return state.concat({
+        id: nextId(state),
         text: action.text,
         completed: false
-      }
-    case 'TOGGLE_TODO':
-      if (state.id !== action.id) {
-        return state
-      }
-
-      return Object.assign({}, state, {
-        completed: !state.completed
       })
-
+    case 'TOGGLE_TODO':
+      return state.map(t => todo(t, action))
     default:
       return state
   }
 }
 
-const todos = (state = [], action) => {
+const nextId = (state) => {
+  const existingIds = state.map(t => t.id)
+  if (existingIds.length === 0) {
+    return 0
+  } else {
+    return Math.max(...existingIds) + 1
+  }
+}
+
+const todo = (state = {}, action) => {
   switch (action.type) {
-    case 'ADD_TODO':
-      return [
-        ...state,
-        todo(undefined, action)
-      ]
     case 'TOGGLE_TODO':
-      return state.map(t => todo(t, action))
+      if (state.id === action.id) {
+        return Object.assign({}, state, {
+          completed: !state.completed
+        })
+      } else {
+        return state
+      }
     default:
       return state
   }
